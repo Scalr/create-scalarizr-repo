058f8af796f651f789f51af009525ea3eea57fe2
Thomas Orozco <thomas@orozco.fr>
2015-03-18 19:43:12 +0100

    Add Amazon Linux aliases (RHEL 6)
    See:
    
    https://github.com/Scalr/int-scalarizr/commit/9b80f1349bfb19100b86c69131b50532cbc4455c
    https://forums.aws.amazon.com/message.jspa?messageID=504160

--- config.sh
+++ config.sh
@@ -11,7 +11,7 @@ REMOTE_REPO_ROOT="http://repo.scalr.net"
 
 RHEL_VERSIONS="5 6 7"
 RHEL_5_ALIASES="5Server"
-RHEL_6_ALIASES="6Server 6.0 6.1 6.2 6.3 6.4 6.5 6.6"
-RHEL_7_ALIASES="7Server 7.0 latest"
+RHEL_6_ALIASES="6Server 6.0 6.1 6.2 6.3 6.4 6.5 2013.03 2013.09 2014.03 2014.09 2015.03 latest"
+RHEL_7_ALIASES="7Server 7.0"
 
 WGET_OPTS=("--recursive" "--no-directories" "--timestamping" "--no-parent" "--no-verbose" "--execute" "robots=off")
