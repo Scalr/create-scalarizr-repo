287b30b623df45d60adedf9e632cc0761f9d6151
Thomas Orozco <thomas@orozco.fr>
2015-04-01 14:15:39 +0200

    Ensure the Nginx PID file is kept unchanged

--- steps/20-configure-nginx.sh
+++ steps/20-configure-nginx.sh
@@ -3,12 +3,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
 source "${SCALR_REPOCONFIG_CONF}"
+
 
 NGINX_USER=""
 
 if [ -f "${NGINX_CONFIG_LOCATION}" ]; then
+  # Figure out where the pid file goes before touching the config file.
+  # We need this to be able to restart Nginx afterwards, and generally to avoid
+  # confusing init code that might depend on it.
+  NGINX_PIDFILE=$(awk -F'[ \t;]+' '/[^#]pid/ {print $2}' "${NGINX_CONFIG_LOCATION}")
+
   for user in ${NGINX_USER_CANDIDATES}; do
     if getent passwd "${user}" >/dev/null; then
       NGINX_USER="${user}"
@@ -17,12 +22,13 @@ if [ -f "${NGINX_CONFIG_LOCATION}" ]; then
   done
 
   if [ -z "${NGINX_USER}" ]; then
-    echo "Unale to find Nginx user. Add yours to NGINX_USER_CANDIDATES in '${SCALR_REPOCONFIG_ROOT}/config.sh'"
+    echo "Unable to find Nginx user. Add yours to NGINX_USER_CANDIDATES in '${SCALR_REPOCONFIG_ROOT}/config.sh'"
     exit 1
   fi
 
   echo "Nginx user is: ${NGINX_USER}"
   cp "${SCALR_REPOCONFIG_ROOT}/files/nginx.conf" "${NGINX_CONFIG_LOCATION}"
+  sed -i "s|__NGINX_PIDFILE__|${NGINX_PIDFILE}|g" "${NGINX_CONFIG_LOCATION}"
   sed -i "s|__LOCAL_REPO_ROOT__|${LOCAL_REPO_ROOT}|g" "${NGINX_CONFIG_LOCATION}"
   sed -i "s/__NGINX_USER__/${NGINX_USER}/g"  "${NGINX_CONFIG_LOCATION}"
 else
