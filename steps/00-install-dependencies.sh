eb88531db46d7e653a327856016bc7b51282aabc
Thomas Orozco <thomas@orozco.fr>
2015-04-01 14:07:32 +0200

    CentOS 6 compatibility (dpkg-devel)

--- steps/00-install-dependencies.sh
+++ steps/00-install-dependencies.sh
@@ -21,9 +21,12 @@ elif which yum; then
     echo "The nux-dextop repo must be enabled (dpkg-dev). Please enable it. View: http://li.nux.ro/repos.html"
   }
 
-  for pkg in "nginx" "createrepo" "wget" "dpkg-dev"; do
+  for pkg in "nginx" "createrepo" "wget"; do
     yum install -y "${pkg}"
   done
+
+  yum install -y "dpkg-devel"
+  yum install -y "dpkg-dev" || true  # on CentOS 6, this is in dpkg-devel
 
 else
   echo "No known package manager found (tried apt-get, yum)."
