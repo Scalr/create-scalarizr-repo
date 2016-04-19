d531518192d41aec2a2d3cd658db4d6965991fee
Marat Komarov <marat@scalr.com>
2016-04-19 13:40:34 +0300

    Added RHEL version aliases

--- config.sh
+++ config.sh
@@ -11,7 +11,7 @@ CLONE_REPOS="stable beta"
 
 REMOTE_REPO_ROOT="http://snapshot.repo.scalr.net/scalarizr/current"
 
-RHEL_VERSIONS="5 5Server 6 6Server 6.0 6.1 6.2 6.3 6.4 6.5 6.6 2013.03 2013.09 2014.03 2014.09 2015.03 7 7Server 7.0 7.1"
+RHEL_VERSIONS="5 5Server 6 6Server 6.0 6.1 6.2 6.3 6.4 6.5 6.6 6.7 2013.03 2013.09 2014.03 2014.09 2015.03 2015.09 2016.03 7 7Server 7.0 7.1 7.2"
 
 WGET_OPTS=("--recursive" "--no-directories" "--timestamping" "--no-parent" "--no-verbose" "--execute" "robots=off")
 
