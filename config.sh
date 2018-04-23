ca8404c016a2ab553a971506bb92f62713a8e5db
Ivan Kovalkovskyi <i.kovalkovskyi@scalr.com>
2018-04-23 19:58:01 +0300

    Support for new apt repository layout

--- config.sh
+++ config.sh
@@ -13,6 +13,10 @@ REMOTE_REPO_ROOT="http://snapshot.repo.scalr.net/scalarizr/current"
 
 RHEL_VERSIONS="6 6Server 6.0 6.1 6.2 6.3 6.4 6.5 6.6 6.7 2013.03 2013.09 2014.03 2014.09 2015.03 2015.09 2016.03 7 7Server 7.0 7.1 7.2"
 
+UBUNTU_VERS="14.04 14.10 16.04 16.10 17.04 17.10 18.04 18.10"
+DEBIAN_VERS="7 8 9"
+APT_BASE_VER="ubuntu/12.04"
+
 WGET_OPTS=("--recursive" "--no-directories" "--timestamping" "--no-parent" "--no-verbose" "--execute" "robots=off")
 
 function wget_mirror() {
--- steps/30-pull-apt-packages.sh
+++ steps/30-pull-apt-packages.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
-set -o errexit
+#set -o errexit
 set -o nounset
 set -o pipefail
 
 source "${SCALR_REPOCONFIG_CONF}"
 
 remote_base="${REMOTE_REPO_ROOT}/apt-plain"
+extra_wget_opts=("--accept" "*.deb")
 
 for repo in ${CLONE_REPOS}; do
-  remote_repo="${remote_base}/${repo}/"
-  extra_wget_opts=("--accept" "*.deb")
+  local_apt_repo="${LOCAL_REPO_ROOT}/${repo}/apt"
+  new_style_repo=false
 
-  echo "Cloning repo '$repo'"
+  echo "Checking for new style apt repository"
+  $(wget -q  ${remote_base}/${repo}/${APT_BASE_VER}/)
+  if [[ $? != 0 ]] ; then
+    echo "Using old style apt repository"
+    remote_repo="${remote_base}/${repo}/"
+    base_apt_repo="${local_apt_repo}"
+  else
+    echo "Using new style apt repository"
+    remote_repo="${remote_base}/${repo}/${APT_BASE_VER}"
+    base_apt_repo="${local_apt_repo}/${APT_BASE_VER}"
+    new_style_repo=true
+  fi
+
+  echo "Cloning repo '$repo' ..."
 
   # Navigate to the appropriate repository
-  cd "${LOCAL_REPO_ROOT}/${repo}/apt"
+  mkdir -p ${base_apt_repo}
+  cd ${base_apt_repo}
 
   # Pull latest packages from Scalr
   wget "${WGET_OPTS[@]}" "${extra_wget_opts[@]}" "${remote_repo}"
@@ -34,4 +49,21 @@ EOC
   dpkg-scansources . > Sources
   cat Packages | gzip -9c > Packages.gz
   cat Sources | gzip -9c > Sources.gz
+
+  if [[ $new_style_repo == true ]] ; then
+    echo "creating Ubuntu symlinks"
+    for ubuntu_ver in ${UBUNTU_VERS}; do
+      ln -sf ${base_apt_repo} ${local_apt_repo}/ubuntu/${ubuntu_ver}
+    done
+
+    echo "creating Debian symlinks"
+    mkdir -p ${local_apt_repo}/debian
+    for debian_ver in ${DEBIAN_VERS}; do
+      ln -sf ${base_apt_repo} ${local_apt_repo}/debian/${debian_ver}
+    done
+
+    echo "Creating symlinks to the root directory"
+    ln -s ${base_apt_repo}/* ${local_apt_repo}
+  fi
+
 done
